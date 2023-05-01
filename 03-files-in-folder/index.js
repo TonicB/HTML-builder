e7a164daca740acdcd031fa8878a89fde7e2db51
TonicB <a.sharafiev@gmail.com>
2023-05-01 21:55:37 +0300

    feat: add solve with promises chain and async/await

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,17 +1,56 @@
 const { stdin, stdout } = process
 const fs = require('fs')
 const path = require('path')
-fs.readdir(
-  path.join(__dirname, 'secret-folder'),
-  {withFileTypes: true},
-  (err, data) => {
-    data.filter( file => file.isFile()).map(file => file.name).forEach( file => {
-      fs.stat(
-        path.join(__dirname,'secret-folder\\') + file,
-        (err, stat) => {
-          console.log(`${file}   ${path.extname(file)}  ${stat.size} kb`)
-        }
-        )
-    })
-  }
-)
+
+// By CALLBACK HELL
+// fs.readdir(
+//   path.join(__dirname, 'secret-folder'),
+//   {withFileTypes: true},
+//   (err, data) => {
+//     data.filter( file => file.isFile()).map(file => file.name).forEach( file => {
+//       fs.stat(
+//         path.join(__dirname,'secret-folder\\') + file,
+//         (err, stat) => {
+//           console.log(`${file}   ${path.extname(file)}  ${stat.size} kb`)
+//         }
+//         )
+//     })
+//   }
+// )
+
+let filesList
+
+const readDir = () => {
+  return new Promise((res, rej) => {
+    fs.readdir(
+        path.join(__dirname, 'secret-folder'),
+        {withFileTypes: true},
+        (err, data) => {
+          if(!err) res(data)
+        })
+  })
+}
+
+const showFileInfo = (file) => {
+  return new Promise((res, rej) => {
+    fs.stat(
+      path.join(__dirname,'secret-folder\\') + file,
+      (err, stat) => {
+        if (!err) console.log(`${file.split('.')[0]}   ${path.extname(file)}  ${stat.size} kb`)
+      }
+    )
+  })
+}
+
+// By promises chain
+// readDir().then((files) => {
+//   files.filter( file => file.isFile()).forEach(file => showFileInfo(file.name))
+// })
+
+// By async/await
+async function printFileInfo () {
+  filesList = await readDir()
+  filesList.filter( file => file.isFile()).forEach(file => showFileInfo(file.name))
+}
+
+printFileInfo()
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,51 @@
+const fs = require('fs')
+const path = require('path')
 
+const makeDir = new Promise((res, rej) => {
+  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {if (err) throw err;})
+  res()
+})
+makeDir.then( () => {
+  return new Promise((res, rej) => {
+    fs.readdir(
+      path.join(__dirname, 'files-copy'),
+      (err, data) => {
+         if (err) throw err
+         res(data)
+        })
+  })
+}).then((files) => {
+  function removeFile(file) {
+    return new Promise((res, rej) => {
+      fs.rm(path.join(__dirname, 'files-copy', file),
+      { force: true },
+      (err) => {if (err) throw err;})
+    })
+  }
+  const promises = files.map(file => removeFile(file))
+   return Promise.all(promises);
+}).then(() => {
+  console.log('yes')
+  return new Promise((res, rej) => {
+    fs.readdir(
+      path.join(__dirname, 'files'),
+      (err, data) => {
+         if (err) throw err
+         res(data)
+        })
+  })
+}).then((files) => {
+  console.log(files)
+  function copyFile(file) {
+    return new Promise((res, rej) => {
+      fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => {if (err) throw err;})
+    })
+  }
+  const promises = files.map(file => copyFile(file))
+  return Promise.all(promises)
+}).then(() => {
+  console.log('All files copied successfully')
+})
+.catch((err) => {
+  console.log('Error:', err)
+});
