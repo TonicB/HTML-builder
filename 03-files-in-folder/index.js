34b034e05b0834801eb0b3f7eac88b29abb97555
TonicB <a.sharafiev@gmail.com>
2023-05-01 11:33:57 +0300

    fix: remove sync function from solve of 3rd task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -6,7 +6,12 @@ fs.readdir(
   {withFileTypes: true},
   (err, data) => {
     data.filter( file => file.isFile()).map(file => file.name).forEach( file => {
-      console.log(`${file}   ${path.extname(file)}    ${fs.statSync(path.join(__dirname,'secret-folder\\') + file).size} kb`)
+      fs.stat(
+        path.join(__dirname,'secret-folder\\') + file,
+        (err, stat) => {
+          console.log(`${file}   ${path.extname(file)}  ${stat.size} kb`)
+        }
+        )
     })
   }
 )
