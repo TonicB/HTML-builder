cdd9868776227daac60c6d5898055bb99fc847bc
TonicB <a.sharafiev@gmail.com>
2023-05-04 08:48:15 +0300

    feat: add final message to task 2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -9,18 +9,24 @@ fs.writeFile(
     // console.log('Файл был создан');
 })
 
-stdout.write('Введите текст, который нужно добавить в файл:')
+stdout.write('Input some text:')
 stdin.on('data', data => {
     if(data.toString().trim() === 'exit') {
+      console.log('Bye')
       process.exit()
     }
    
     fs.appendFile(
       path.join(__dirname, 'text.txt'),
-      data.toString().trim(),
+      data.toString(),
       err => {
         if (err) throw err
       }
       )
   // }
+})
+
+process.on('SIGINT', () => {
+  console.log('Bye')
+  process.exit()
 })
